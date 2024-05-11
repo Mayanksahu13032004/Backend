f958c41790cc580502747e6cbd7665251f254dfb
Mayanksahu13032004 <mayanksahu13032004.com>
2024-05-11 07:35:13 +0530

    connection successful

---
+++ src/index.js
@@ -0,0 +1,84 @@
+// shree ganeshya nahyma
+// database connection
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  import express from "express"
+//  const app=express()
+
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listining on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.log("Error:",error);
+//         throw err
+//     }
+// })()
