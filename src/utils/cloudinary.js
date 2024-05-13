650f0df165201f8e946da49c09867e8f1be9b92f
Mayanksahu13032004 <mayanksahu13032004.com>
2024-05-13 10:09:51 +0530

    multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+// fs means file system
+import fs from "fs"
+
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary=async(localFilePath)=>{
+    try {
+        if (!localFilePath) return null
+        // upload file on clodnary
+        const resposne= await cloudinary.uploader.upload(localFilePath,{
+          resource_type:"auto"
+        })
+        // file has been uploaded successfully 
+        console.log("File is uplosded on cloudanay",resposne.url);
+        return resposne;
+    } catch (error) {
+      // remove he locally saved tempary file as the upload operation got file
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
