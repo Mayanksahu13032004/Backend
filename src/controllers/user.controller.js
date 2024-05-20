316f73785738c84dce719089acb14c8245c1da41
Mayanksahu13032004 <mayanksahu13032004.com>
2024-05-20 18:16:40 +0530

    model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -181,8 +181,9 @@ const logoutUser=asyncHandler(async(req,res)=>{
             await User.findByIdAndUpdate(
                 req.user._id,
                 {
-                    $set:{
-                        refreshToken:undefined
+                    $unset:{
+                        // this  remove filed from document
+                        refreshToken:1
                     }
                 },
                 {
