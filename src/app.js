70e052fa1c96ae25cc5eb6bd4a925100603b79a3
Mayanksahu13032004 <mayanksahu13032004.com>
2024-05-30 19:15:25 +0530

    model

--- src/app.js
+++ src/app.js
@@ -18,15 +18,16 @@ app.use(cookieParser())
 
 // routes import
 import userRouter from './routes/user.routes.js'
-import  healthcheck  from "./routes/healthcheck.routes.js"
+import  healthRouter from "./routes/healthcheck.routes.js"
+import playlistrouter from "./routes/playlist.routes.js"
 
 
 
 
 // routes declaration
 app.use("/api/v1/users",userRouter)
-app.use("/api/v1/condition",healthcheck)
-
+app.use("/api/v1/condition",healthRouter)
+app.use("/api/v1/list",playlistrouter)
 // http://localhost:8000/api/v1/users/register
 
 export { app }
